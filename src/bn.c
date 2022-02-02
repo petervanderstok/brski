440476ba5b8c5d5c7fd0d834dd7a667990bbe048
peter van der stok <stokcons@bbhmail.nl>
2022-02-02 12:14:09 +0100

    repaired bignum_to_string

--- src/bn.c
+++ src/bn.c
@@ -183,32 +183,30 @@ void bignum_to_string(struct bn* n, char* str, int nbytes)
   require(nbytes > 0, "nbytes must be positive");
   require((nbytes & 1) == 0, "string format must be in hex -> equal number of bytes");
 
-  int j = BN_ARRAY_SIZE - 1; /* index into array - reading "MSB" first -> big-endian */
-  int i = 0;                 /* index into string representation. */
-
+  int j = (nbytes/(2*WORD_SIZE)) - 1;        /* index into array - reading "LSB" first -> big-endian */
+  int i = 0;                           /* index into string representation. */
+  
+  /* initialize string  to zeroes */
+  for (int qq = 0 ; qq < nbytes; qq++) str[qq] = '0';
   /* reading last array-element "MSB" first -> big endian */
-  while ((j >= 0) && (nbytes > (i + 1)))
+  while ((j > -1) && (i < nbytes + 1 - (2*WORD_SIZE)))
   {
     sprintf(&str[i], SPRINTF_FORMAT_STR, n->array[j]);
-    i += (2 * WORD_SIZE); /* step WORD_SIZE hex-byte(s) forward in the string. */
-    j -= 1;               /* step one element back in the array. */
+    i +=  (2*WORD_SIZE);      /* step WORD_SIZE hex-byte(s) forward in the string. */
+    j -= 1;                   /* step one element back in the array. */
   }
 
   /* Count leading zeros: */
   j = 0;
-  while (str[j] == '0')
-  {
-    j += 1;
-  }
- 
+  while ((str[j] == '0') && (j < nbytes))j++;
   /* Move string j places ahead, effectively skipping leading zeros */ 
-  for (i = 0; i < (nbytes - j); ++i)
+  for (i = 0; i < (nbytes - j); i++)
   {
     str[i] = str[i + j];
   }
 
   /* Zero-terminate string */
-  str[i] = 0;
+  str[nbytes - j] = 0;
 }
 
 
